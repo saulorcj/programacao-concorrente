4742aa74fb46296fba9934fb5d99ec0bb3f5cdce
Saulo Roberto <sauloroberto1000@hotmail.com>
2022-09-01 19:06:25 -0300

    hello_world v2

--- hello_pc.c
+++ hello_pc.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define NTHREADS 10
+
 // função que a thread irá executar
 void* tarefa(void* arg){
-    printf("Ola, sou a thread nova!\n");
+    int ident = *((int*) arg);
+
+    printf("Ola, sou a thread %d!\n", ident);
     pthread_exit(NULL);
 }
 
-// dunção principal
+// função principal
 int main(){
-    pthread_t tid; // identificador da thread no sistema
+    pthread_t tid[NTHREADS]; // identificador da thread no sistema
 
-    if (pthread_create(&tid, NULL, tarefa, NULL)) // cria a thread nova
-        printf("ERRO -- pthread_create\n");
+    int ident[NTHREADS]; // identificador local da thread
 
-    printf("Ola, sou a thread princial!\n");
+    // cria as threads novas
+    for(int i = 0;i < NTHREADS; i++){
+        ident[i] = i;
+        if (pthread_create(&tid[i], NULL, tarefa, (void*) &ident[i]))
+            printf("ERRO -- pthread_create %d\n", i);
+    }
+
+    printf("Ola, sou a thread principal!\n");
 
     pthread_exit(NULL); // desvincula o término da main do término da thread
 
